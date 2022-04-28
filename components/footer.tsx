323c5e97ac8e47fb42875364d11aecbf36b011b5
Jeff See <jeffsee.55@gmail.com>
2022-04-28 16:18:51 -0700

    Remove failing type check

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-import { DisplayText } from './typographqy'
-import { useRouter } from 'next/router'
-import { Markdown } from './markdown'
-import Link from 'next/link'
-import type { TinaCollection } from 'tinacms'
-import { Selector } from '../zeus'
-import { Response } from './util'
+import React from "react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { DisplayText } from "./typographqy";
+import { useRouter } from "next/router";
+import { Markdown } from "./markdown";
+import Link from "next/link";
+import type { TinaCollection } from "tinacms";
+import { Selector } from "../zeus";
+import { Response } from "./util";
 
-export const footerQuery = Selector('Footer')({
+export const footerQuery = Selector("Footer")({
   offices: {
     address: true,
     location: true,
@@ -17,68 +17,68 @@ export const footerQuery = Selector('Footer')({
   disclaimers: {
     body: true,
   },
-})
+});
 
-type FooterProps = Response<'Footer', typeof footerQuery>
-type Office = FooterProps['offices'][number]
+type FooterProps = Response<"Footer", typeof footerQuery>;
+type Office = FooterProps["offices"][number];
 
 export const footerTemplate = (): TinaCollection => {
   return {
-    label: 'Footer',
-    name: 'footer',
-    path: 'content/footer',
+    label: "Footer",
+    name: "footer",
+    path: "content/footer",
     fields: [
       {
-        label: 'Offices',
-        name: 'offices',
-        type: 'object',
+        label: "Offices",
+        name: "offices",
+        type: "object",
         list: true,
         ui: {
           defaultItem: {
-            location: 'London',
-            address: 'Some Address\nInLondon\nUnited Kingdom',
-            phone: '+44 123 456',
+            location: "London",
+            address: "Some Address\nInLondon\nUnited Kingdom",
+            phone: "+44 123 456",
           },
         },
         fields: [
           {
-            label: 'Location',
-            name: 'location',
+            label: "Location",
+            name: "location",
             required: true,
-            type: 'string',
+            type: "string",
           },
           {
-            label: 'Address',
-            name: 'address',
+            label: "Address",
+            name: "address",
             required: true,
-            type: 'string',
+            type: "string",
           },
           {
-            label: 'Phone',
-            name: 'phone',
+            label: "Phone",
+            name: "phone",
             required: true,
-            type: 'string',
+            type: "string",
           },
         ],
       },
       {
-        label: 'Disclaimers',
-        name: 'disclaimers',
+        label: "Disclaimers",
+        name: "disclaimers",
         // @ts-ignore
-        required: true,
-        type: 'object',
+        // required: true,
+        type: "object",
         list: true,
         fields: [
           {
-            label: 'Body',
-            name: 'body',
-            type: 'rich-text',
+            label: "Body",
+            name: "body",
+            type: "rich-text",
           },
         ],
       },
     ],
-  }
-}
+  };
+};
 
 export function Footer(props: FooterProps) {
   return (
@@ -95,7 +95,7 @@ export function Footer(props: FooterProps) {
                 <Markdown key={i} classNames="italic" variant="small">
                   {disclaimer.body}
                 </Markdown>
-              )
+              );
             })}
           </div>
           <CountrySelector />
@@ -121,24 +121,24 @@ export function Footer(props: FooterProps) {
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export function CountrySelector2() {
-  const router = useRouter()
+  const router = useRouter();
   const locales = {
-    'en-us': 'US',
-    'en-gb': 'UK',
-    'en-au': 'AU',
-  }
+    "en-us": "US",
+    "en-gb": "UK",
+    "en-au": "AU",
+  };
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
@@ -165,13 +165,13 @@ export function CountrySelector2() {
                   type="button"
                   onClick={() => {
                     router.push(router.asPath, null, {
-                      locale: 'en-au',
+                      locale: "en-au",
                       scroll: false,
-                    })
+                    });
                   }}
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   AU
@@ -183,15 +183,15 @@ export function CountrySelector2() {
                 <button
                   type="button"
                   onClick={() => {
-                    console.log('doit')
+                    console.log("doit");
                     router.push(router.asPath, null, {
-                      locale: 'en-us',
+                      locale: "en-us",
                       scroll: false,
-                    })
+                    });
                   }}
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   US
@@ -204,13 +204,13 @@ export function CountrySelector2() {
                   type="button"
                   onClick={() => {
                     router.push(router.asPath, null, {
-                      locale: 'en-gb',
+                      locale: "en-gb",
                       scroll: false,
-                    })
+                    });
                   }}
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   UK
@@ -221,7 +221,7 @@ export function CountrySelector2() {
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
 
 export const CountrySelector = () => {
@@ -241,8 +241,8 @@ export const CountrySelector = () => {
         </fieldset>
       </form>
     </div>
-  )
-}
+  );
+};
 
 const LinkedIn = () => {
   return (
@@ -257,8 +257,8 @@ const LinkedIn = () => {
         d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
       />
     </svg>
-  )
-}
+  );
+};
 
 /* This example requires Tailwind CSS v2.0+ */
 export function Offices({ offices }: { offices?: Office[] }) {
@@ -277,7 +277,7 @@ export function Offices({ offices }: { offices?: Office[] }) {
                   {office.location}
                 </h3>
                 <p
-                  style={{ whiteSpace: 'pre-line' }}
+                  style={{ whiteSpace: "pre-line" }}
                   className="mt-2 text-base text-gray-50"
                 >
                   {office.address}
@@ -291,12 +291,12 @@ export function Offices({ offices }: { offices?: Office[] }) {
                   </a>
                 </span>
               </div>
-            )
+            );
           })}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 /*
@@ -317,33 +317,33 @@ export function Offices({ offices }: { offices?: Office[] }) {
 */
 const navigation = {
   solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
+    { name: "Marketing", href: "#" },
+    { name: "Analytics", href: "#" },
+    { name: "Commerce", href: "#" },
+    { name: "Insights", href: "#" },
   ],
   support: [
-    { name: 'Pricing', href: '#' },
-    { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
+    { name: "Pricing", href: "#" },
+    { name: "Documentation", href: "#" },
+    { name: "Guides", href: "#" },
+    { name: "API Status", href: "#" },
   ],
   company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: "About", href: "#" },
+    { name: "Blog", href: "#" },
+    { name: "Jobs", href: "#" },
+    { name: "Press", href: "#" },
+    { name: "Partners", href: "#" },
   ],
   legal: [
-    { name: 'Claim', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
+    { name: "Claim", href: "#" },
+    { name: "Privacy", href: "#" },
+    { name: "Terms", href: "#" },
   ],
   social: [
     {
-      name: 'Facebook',
-      href: '#',
+      name: "Facebook",
+      href: "#",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -355,8 +355,8 @@ const navigation = {
       ),
     },
     {
-      name: 'Instagram',
-      href: '#',
+      name: "Instagram",
+      href: "#",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -368,8 +368,8 @@ const navigation = {
       ),
     },
     {
-      name: 'Twitter',
-      href: '#',
+      name: "Twitter",
+      href: "#",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -377,8 +377,8 @@ const navigation = {
       ),
     },
     {
-      name: 'GitHub',
-      href: '#',
+      name: "GitHub",
+      href: "#",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -390,8 +390,8 @@ const navigation = {
       ),
     },
     {
-      name: 'Dribbble',
-      href: '#',
+      name: "Dribbble",
+      href: "#",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -403,7 +403,7 @@ const navigation = {
       ),
     },
   ],
-}
+};
 
 export default function Example() {
   return (
@@ -600,5 +600,5 @@ export default function Example() {
         </div>
       </div>
     </footer>
-  )
+  );
 }
